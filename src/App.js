afbbab3c79b91ba90902ae0a8a58422a5ced8053
Petros Palioudakis <xaniapaliou@gmail.com>
2022-12-08 16:21:51 +0200

    add first routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { Route, Routes } from 'react-router-dom';
+import { Login } from './components/Login';
+import { Home } from './container/Home';
+
 function App() {
-  return <div className={'text-3xl font-bold underline'}>Hello from App</div>;
+  return (
+    <Routes>
+      <Route path={'login'} element={<Login />} />
+      <Route path={'/*'} element={<Home />} />
+    </Routes>
+  );
 }
 
 export default App;
